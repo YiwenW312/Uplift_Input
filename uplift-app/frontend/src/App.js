5fcf1ed12c5c34537a2fab1359b09d5d4fe13e01
Yiwen Wang <124082584+YiwenW312@users.noreply.github.com>
2024-07-24 18:23:03 -0700

    fix frontend setup

--- uplift-app/frontend/src/App.js
+++ uplift-app/frontend/src/App.js
@@ -1,17 +1,17 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import AddParticipant from './pages/AddParticipant';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/add-participant" component={AddParticipant} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add-participant" element={<AddParticipant />} />
         {/* Define other routes similarly */}
-      </Switch>
+      </Routes>
     </Router>
   );
 };
